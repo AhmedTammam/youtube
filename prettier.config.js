a2ebbafec23794c7b3df05059b222d6e4e7a0fe7
ahmed tammam <tamam65@gmail.com>
2021-08-05 06:54:13 +0200

    - initial app - install necessary dependencies - config prettier - remove unused files

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable strict, import/no-commonjs */
+'use strict';
+
+module.exports = {
+    printWidth: 120,
+    trailingComma: 'none',
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: 'lf',
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid'
+};
